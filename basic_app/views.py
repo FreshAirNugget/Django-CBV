936cb220314758d683ff85bff5d8cdf0014f426f
Raditya <Gatra.Raditya@edelman.com>
2018-10-02 18:25:15 +0700

    basic list and detail view

--- basic_app/views.py
+++ basic_app/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import View,TemplateView
+from django.views.generic import View,TemplateView,DetailView,ListView
+from . import models
 # Create your views here.
 
-class IndexView(TemplateView):
-    template_name = 'basic_app/index.html'
+class SchoolListView(ListView):
+    model = models.School
+    template_name = 'basic_app/school_list.html'
 
-    def get_context_data(self,**kwargs):
-        context = super().get_context_data(**kwargs)
-        context['inject_me'] = 'HEY! I\'M INJECTED'
-        return context
+class SchoolDetailView(DetailView):
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
+
+
+
+# class IndexView(TemplateView):
+#     template_name = 'basic_app/index.html'
+#
+#     def get_context_data(self,**kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['inject_me'] = 'HEY! I\'M INJECTED'
+#         return context
